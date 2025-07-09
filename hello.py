f9666b718ed6bb00fc49de459b3ce92999e249aa
Raj <manish.raj.ext@siemens.com>
2025-07-09 22:11:39 +0530

    addded

--- hello.py
+++
@@ -1,5 +0,0 @@
-print("hello world")
-# This is a simple Python script that prints "hello world" to the console.
-# It serves as a basic example of how to write and run a Python program.    
-# To run this script, save it as hello.py and execute it using the command:
-# python hello.py
