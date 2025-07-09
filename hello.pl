8f935bd0cfed1302f4fba659a81beead3d83788b
Raj <manish.raj.ext@siemens.com>
2025-07-09 21:38:25 +0530

    addded

--- hello.pl
+++ hello.pl
@@ -1,28 +1,6 @@
 #!/usr/bin/perl
 
 print("Hello, World!\n");
-# This is a simple Perl script that prints "Hello, World!" to the console.
-# To run this script, save it as hello.pl and execute it using the command:
-# perl hello.pl
 print("This is a Perl script.\n");
-# You can modify this script to perform other tasks as needed.  
-# For example, you can add more print statements or include logic to handle user input.
-# Perl is a versatile scripting language that can be used for various tasks, including text processing, 
-# system administration, and web development.
-# Make sure you have Perl installed on your system to run this script.  
 print("Perl is great for text processing!\n");
-# You can also use Perl modules to extend the functionality of your scripts.   
-# For example, you can use the 'LWP::UserAgent' module to make web requests or 'DBI' for database interactions.
-# To learn more about Perl, you can refer to the official documentation at https://perldoc.perl.org/.
 print("Happy coding with Perl!\n");
-# Remember to check for syntax errors and test your scripts thoroughly.
-# Perl has a rich ecosystem of libraries and frameworks that can help you build complex applications.
-# Enjoy exploring the world of Perl programming!
-# If you have any questions or need help, there are many online communities and forums where you
-# can seek assistance from other Perl developers.
-# Keep learning and improving your Perl skills! 
-# Perl is a powerful language that has been around for decades and continues to be widely used.
-# Whether you're a beginner or an experienced programmer, Perl has something to offer for everyone.
-# Don't forget to explore CPAN (Comprehensive Perl Archive Network) for a vast collection of Perl modules.
-# CPAN is a great resource for finding reusable code and libraries that can save you time and effort in your projects.
-# Happy coding!
